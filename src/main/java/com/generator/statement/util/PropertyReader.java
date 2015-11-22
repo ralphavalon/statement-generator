0a38739ab5a57f96fc411f447ab0f60b70024010
Ralph Avalon <amoedo.raphael@gmail.com>
2015-11-22 13:38:23 -0200

    Custom config file #2

--- src/main/java/com/generator/statement/util/PropertyReader.java
+++ src/main/java/com/generator/statement/util/PropertyReader.java
@@ -1,21 +1,26 @@
 package com.generator.statement.util;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Properties;
 
 public class PropertyReader {
 	
-	private static Properties prop = new Properties();
+	private static Properties defaultProperties = new Properties();
+	private static Properties customProperties = new Properties();
 	 
 	static {
 		try {
-			prop.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties"));
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+			defaultProperties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties"));
+			customProperties.load(new FileInputStream(new File("config.properties")));
+		}catch(Exception e){}
 	}
 	
 	public static String getProperty(String key){
-		return prop.getProperty(key);
+		if(customProperties.getProperty(key) == null) {
+			return defaultProperties.getProperty(key);
+		}
+		return customProperties.getProperty(key);
 	}
 
 }
